75821895d0b6586e6c44d506637043de361472b0
Lea Vasiljevic <leavasiljevic@gmail.com>
2022-08-24 15:21:55 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+import { useState } from 'react';
+
+function App() {
+  const [sound,setSound] = useState("");
+  const [color,setColor] = useState("#000000");
+
+  const submit = (e) => {
+    e.preventDefault();
+
+    alert(`${sound} sound like ${color}`);
+    setSound("");
+    setColor("#000000");
+  }
+
+  return (
+    <form onSubmit={submit}>
+      <input value={sound} type="text" placeholder="Sound..."
+        onChange={(e) => setSound(e.target.value)}/>
+      <input value={color} type="color"
+        onChange={(e) => setColor(e.target.value)}/>
+      <button> ADD </button>
+    </form>
+
+  );
+}
+
+export default App;
